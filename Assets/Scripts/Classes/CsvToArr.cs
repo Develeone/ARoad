ffd74cc85fb8ade1ccf26754fe10c9c3a7b9fe86
Pleshanov <pleshanov@so-vl.ru>
2016-09-23 20:14:15 +1000

    Вынес парс csv файла координат в список флоатов

---
+++ Assets/Scripts/Classes/CsvToArr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class CsvToArr : MonoBehaviour {
+
+    public List<List<float>> data = new List<List<float>>();
+
+    public CsvToArr ()
+    {
+        string filePath = @"Assets\Resources\cams.csv";
+        StreamReader file = new StreamReader(filePath);
+
+        while (!file.EndOfStream)
+        {
+            String[] line = file.ReadLine().Split(';');
+            List<float> coordinate = new List<float>();
+
+            foreach (String elem in line)
+                coordinate.Add(float.Parse(elem));
+
+            data.Add(coordinate);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectsPlacer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectsPlacer : MonoBehaviour {
+
+    public CsvToArr camCoordinates;
+
+	// Use this for initialization
+	void Start () {
+        camCoordinates = new CsvToArr();
+        Debug.Log(camCoordinates.data[0][0]);
+        Debug.Log(camCoordinates.data[0][1]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
