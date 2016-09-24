335a94fee6b0552e57a9689a2c6a959edf2cd35b
Pleshanov <pleshanov@so-vl.ru>
2016-09-24 17:40:04 +1000

    Сделал инстанс пит стопов

--- Assets/Scripts/ObjectsPlacer.cs
+++ Assets/Scripts/ObjectsPlacer.cs
@@ -11,35 +11,41 @@ public class ObjectsPlacer : MonoBehaviour
     public List<Vector2> sceneCamCoordinates        = new List<Vector2>();
     public List<Vector2> sceneGasStationCoordinates = new List<Vector2>();
     public List<Vector2> sceneParkingCoordinates    = new List<Vector2>();
+    public List<Vector2> scenePitStopCoordinates    = new List<Vector2>();
 
     public GameObject speedCamPointer;
     public GameObject gasStationPointer;
     public GameObject parkingPointer;
+    public GameObject pitStopPointer;
 
     public TextAsset camsFile;
     public TextAsset gasolinesFile;
     public TextAsset parkingFile;
+    public TextAsset pitStopFile;
 
     public Transform camPointersParent;
     public Transform gasStationPointerParent;
     public Transform parkingPointerParent;
+    public Transform pitStopPointerParent;
 
     int camPointersCount        = 0;
     int gasStationPointersCount = 0;
     int parkingPointersCount    = 0;
+    int pitStopPointersCount    = 0;
 
 
-    
+
     IEnumerator Start()
     {
-        List<float[]> gasolinesPrises           = CsvParser.ParceCsvPrises(gasolinesFile);
+        gasolinesPrises           = CsvParser.ParceCsvPrises(gasolinesFile);
 
         while (!GpsTracking.GpsReady)
             yield return new WaitForSeconds(1);
 
-        sceneCamCoordinates = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(camsFile));
+        sceneCamCoordinates        = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(camsFile));
         sceneGasStationCoordinates = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(gasolinesFile));
-        sceneParkingCoordinates = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(parkingFile));
+        sceneParkingCoordinates    = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(parkingFile));
+        scenePitStopCoordinates    = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(pitStopFile));
     }
 
     void OnGUI()
@@ -49,6 +55,7 @@ public class ObjectsPlacer : MonoBehaviour
         GUILayout.Label("Camers instantiated: " + camPointersCount);
         GUILayout.Label("Gas Station instantiated: " + gasStationPointersCount);
         GUILayout.Label("Parking instantiated: " + parkingPointersCount);
+        GUILayout.Label("PitStop instantiated: " + pitStopPointersCount);
     }
     
     void FixedUpdate()
@@ -56,48 +63,12 @@ public class ObjectsPlacer : MonoBehaviour
         if (!GpsTracking.GpsReady)
             return;
 
-        foreach (Vector2 elem in sceneCamCoordinates)
-        {
-            Vector3 currentCam     = new Vector3(elem.x, 5, elem.y);
-            float distanceToObject = Math.Abs(Vector3.Distance(new Vector3(GpsTracking.currentCoordinate.longitude, 5, GpsTracking.currentCoordinate.latitude), currentCam));
+        InstancePointer(sceneCamCoordinates, 500, speedCamPointer, camPointersParent, ref camPointersCount, false);
+        InstancePointer(sceneGasStationCoordinates, 2000, gasStationPointer, gasStationPointerParent, ref gasStationPointersCount, true);
+        InstancePointer(sceneParkingCoordinates, 1000, parkingPointer, parkingPointerParent, ref parkingPointersCount, false);
+        InstancePointer(scenePitStopCoordinates, 1000, pitStopPointer, pitStopPointerParent, ref pitStopPointersCount, false);
 
-            if (distanceToObject < 500)
-            {
-                GameObject newCamPointer       = (GameObject)GameObject.Instantiate(speedCamPointer, currentCam, Quaternion.identity);
-                newCamPointer.transform.parent = camPointersParent;
-                camPointersCount++;
-            }
-        }
-
-        for (int i = 0; i < sceneGasStationCoordinates.Count; i++)
-        {
-            Vector3 currentGasStation = new Vector3(sceneGasStationCoordinates[i].x, 5, sceneGasStationCoordinates[i].y);
-            float distanceToObject    = Math.Abs(Vector3.Distance(new Vector3(GpsTracking.currentCoordinate.longitude, 5, GpsTracking.currentCoordinate.latitude), currentGasStation));
-
-            if (distanceToObject < 3000)
-            {
-                GameObject newGasStationPointer = (GameObject)GameObject.Instantiate(gasStationPointer, currentGasStation, Quaternion.identity);
-                TextMesh priceTextMesh = newGasStationPointer.GetComponentInChildren<TextMesh>();
-                priceTextMesh.text = gasolinesPrises[i][0] + "р";
-                newGasStationPointer.transform.parent = gasStationPointerParent;
-                gasStationPointersCount++;
-            }
-        }
-
-        foreach (Vector2 elem in sceneParkingCoordinates)
-        {
-            Vector3 currentParking = new Vector3(elem.x, 5, elem.y);
-            float distanceToObject = Math.Abs(Vector3.Distance(new Vector3(GpsTracking.currentCoordinate.longitude, 5, GpsTracking.currentCoordinate.latitude), currentParking));
-
-            if (distanceToObject < 1000)
-            {
-                GameObject newParkingPointer = (GameObject)GameObject.Instantiate(parkingPointer, currentParking, Quaternion.identity);
-                newParkingPointer.transform.parent = parkingPointerParent;
-                parkingPointersCount++;
-            }
-        }
     }
-
 
     List<Vector2> worldToSceneCoordinates(List<Coordinate> worldCoordinateList)
     {
@@ -107,6 +78,31 @@ public class ObjectsPlacer : MonoBehaviour
             sceneCoordinateList.Add(CoordinatesConverter.ConvertCoordinate(elem));
 
         return sceneCoordinateList;
+    }
+
+    void InstancePointer(List<Vector2> sceneCoordinateList, int requiredDistance, GameObject currentPointer, Transform pointerParent, ref int counter, bool setPointerText)
+    {
+
+        for (int i = 0; i < sceneCoordinateList.Count; i++)
+        {
+            Vector3 currentPointerPosition = new Vector3(sceneCoordinateList[i].x, 5, sceneCoordinateList[i].y);
+            float distanceToObject = Math.Abs(Vector3.Distance(new Vector3(GpsTracking.currentCoordinate.longitude, 5, GpsTracking.currentCoordinate.latitude), currentPointerPosition));
+
+            if (distanceToObject < requiredDistance)
+            {
+                GameObject newPointer = (GameObject)GameObject.Instantiate(currentPointer, currentPointerPosition, Quaternion.identity);
+
+                if (setPointerText)
+                {
+                    TextMesh priceTextMesh = newPointer.GetComponentInChildren<TextMesh>();
+                    priceTextMesh.text = gasolinesPrises[i][0] + "р";
+                }
+
+                newPointer.transform.parent = pointerParent;
+                counter++;
+            }
+        }
+    
     }
 
 }
