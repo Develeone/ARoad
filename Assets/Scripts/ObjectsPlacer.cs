6834a0c438952fa136bcfe438c7065b5bde81366
Pleshanov <pleshanov@so-vl.ru>
2016-09-25 00:34:48 +1000

    Добавлен вывод маркеров дтп, сообщений, гаи

--- Assets/Scripts/ObjectsPlacer.cs
+++ Assets/Scripts/ObjectsPlacer.cs
@@ -7,24 +7,34 @@ using System;
 public class ObjectsPlacer : MonoBehaviour
 {
     public List<float[]> gasolinesPrises;
+    public List<string> Messages;
 
     public List<Vector2> sceneCamCoordinates        = new List<Vector2>();
     public List<Vector2> sceneGasStationCoordinates = new List<Vector2>();
     public List<Vector2> sceneParkingCoordinates    = new List<Vector2>();
     public List<Vector2> scenePitStopCoordinates    = new List<Vector2>();
     public List<Vector2> sceneTiresCoordinates      = new List<Vector2>();
+    public List<Vector2> sceneCrashCoordinates      = new List<Vector2>();
+    public List<Vector2> scenePoliceCoordinates     = new List<Vector2>();
+    public List<Vector2> sceneMessageCoordinates    = new List<Vector2>();
 
     public GameObject[] instantietedCams;
     public GameObject[] instantietedGasStations;
     public GameObject[] instantietedParkings;
     public GameObject[] instantietedPitStops;
     public GameObject[] instantietedTires;
+    public GameObject[] instantietedCrash;
+    public GameObject[] instantietedPolice;
+    public GameObject[] instantietedMessage;
 
     public GameObject speedCamPointer;
     public GameObject gasStationPointer;
     public GameObject parkingPointer;
     public GameObject pitStopPointer;
     public GameObject tiresPointer;
+    public GameObject crashPointer;
+    public GameObject policePointer;
+    public GameObject messagePointer;
 
     public TextAsset camsFile;
     public TextAsset gasolinesFile;
@@ -37,12 +47,9 @@ public class ObjectsPlacer : MonoBehaviour
     public Transform parkingPointerParent;
     public Transform pitStopPointerParent;
     public Transform tiresPointerParent;
-
-    int camPointersCount        = 0;
-    int gasStationPointersCount = 0;
-    int parkingPointersCount    = 0;
-    int pitStopPointersCount    = 0;
-    int tiresPointersCount      = 0;
+    public Transform crashPointerParent;
+    public Transform policePointerParent;
+    public Transform messagePointerParent;
 
     IEnumerator Start()
     {
@@ -50,9 +57,6 @@ public class ObjectsPlacer : MonoBehaviour
 
         while (!GpsTracking.GpsReady)
             yield return new WaitForSeconds(1);
-
-        PlayerPrefs.SetInt("showCam", 1);
-        PlayerPrefs.SetInt("showTires", 1);
 
         if (PlayerPrefs.GetInt("showCam") == 1)
             sceneCamCoordinates        = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(camsFile));
@@ -66,7 +70,7 @@ public class ObjectsPlacer : MonoBehaviour
         if (PlayerPrefs.GetInt("showPitStop") == 1)
             scenePitStopCoordinates    = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(pitStopFile));
 
-        if (PlayerPrefs.GetInt("showTires") == 1)
+        if (PlayerPrefs.GetInt("showTire") == 1)
             sceneTiresCoordinates      = worldToSceneCoordinates(CsvParser.ParseCsvCoordinates(tiresFile));
 
         instantietedCams        = new GameObject[sceneCamCoordinates.Count];
@@ -74,17 +78,15 @@ public class ObjectsPlacer : MonoBehaviour
         instantietedParkings    = new GameObject[sceneParkingCoordinates.Count];
         instantietedPitStops    = new GameObject[scenePitStopCoordinates.Count];
         instantietedTires       = new GameObject[sceneTiresCoordinates.Count];
+        instantietedCrash       = new GameObject[sceneCrashCoordinates.Count];
+        instantietedPolice      = new GameObject[scenePoliceCoordinates.Count];
+        instantietedMessage     = new GameObject[sceneMessageCoordinates.Count];
     }
 
     void OnGUI()
     {
         GUILayout.Label("");
         GUILayout.Label("");
-        GUILayout.Label("Camers instantiated: " + camPointersCount);
-        GUILayout.Label("Gas Station instantiated: " + gasStationPointersCount);
-        GUILayout.Label("Parking instantiated: " + parkingPointersCount);
-        GUILayout.Label("PitStop instantiated: " + pitStopPointersCount);
-        GUILayout.Label("Tires instantiated: " + tiresPointersCount);
     }
     
     void FixedUpdate()
@@ -92,11 +94,14 @@ public class ObjectsPlacer : MonoBehaviour
         if (!GpsTracking.GpsReady)
             return;
 
-        InstancePointer(sceneCamCoordinates, 500, speedCamPointer, camPointersParent, ref camPointersCount, false, instantietedCams);
-        InstancePointer(sceneGasStationCoordinates, 1000, gasStationPointer, gasStationPointerParent, ref gasStationPointersCount, true, instantietedGasStations);
-        InstancePointer(sceneParkingCoordinates, 1000, parkingPointer, parkingPointerParent, ref parkingPointersCount, false, instantietedParkings);
-        InstancePointer(scenePitStopCoordinates, 1000, pitStopPointer, pitStopPointerParent, ref pitStopPointersCount, false, instantietedPitStops);
-        InstancePointer(sceneTiresCoordinates, 1000, tiresPointer, tiresPointerParent, ref tiresPointersCount, false, instantietedTires);
+        InstancePointer(sceneCamCoordinates, 500, speedCamPointer, camPointersParent, false, instantietedCams);
+        InstancePointer(sceneGasStationCoordinates, 1000, gasStationPointer, gasStationPointerParent, true, instantietedGasStations);
+        InstancePointer(sceneParkingCoordinates, 1000, parkingPointer, parkingPointerParent, false, instantietedParkings);
+        InstancePointer(scenePitStopCoordinates, 1000, pitStopPointer, pitStopPointerParent, false, instantietedPitStops);
+        InstancePointer(sceneTiresCoordinates, 1000, tiresPointer, tiresPointerParent, false, instantietedTires);
+        InstancePointer(sceneCrashCoordinates, 1000, crashPointer, crashPointerParent, false, instantietedCrash);
+        InstancePointer(scenePoliceCoordinates, 1000, policePointer, policePointerParent, false, instantietedPolice);
+        InstancePointer(sceneMessageCoordinates, 1000, messagePointer, messagePointerParent, false, instantietedMessage);
 
     }
 
@@ -110,7 +115,7 @@ public class ObjectsPlacer : MonoBehaviour
         return sceneCoordinateList;
     }
 
-    void InstancePointer(List<Vector2> sceneCoordinateList, int requiredDistance, GameObject currentPointer, Transform pointerParent, ref int counter, bool setPointerText, GameObject[] instantietedPointerList)
+    void InstancePointer(List<Vector2> sceneCoordinateList, int requiredDistance, GameObject currentPointer, Transform pointerParent, bool setPointerText, GameObject[] instantietedPointerList)
     {
 
         for (int i = 0; i < sceneCoordinateList.Count; i++)
@@ -134,23 +139,16 @@ public class ObjectsPlacer : MonoBehaviour
 
                 if (setPointerText)
                 {
-<<<<<<< HEAD
                     TextMesh priceTextMesh = newPointer.GetComponentInChildren<TextMesh>();
                     priceTextMesh.text = gasolinesPrises[i][PlayerPrefs.GetInt("gasolineType")] + "р";
-=======
-                    TextMesh priceTextMesh = textMeshes[distanceIndex];
-                    priceTextMesh.text = gasolinesPrises[i][0] + "р";
->>>>>>> 158c45f0dd1c2d566a0923f602d9016788addcc8
                 }
 
                 newPointer.transform.parent = pointerParent;
-                counter++;
             }
             else if (distanceToObject > requiredDistance && instantietedPointerList[i] != null)
             {
                 GameObject.Destroy(instantietedPointerList[i]);
                 instantietedPointerList[i] = null;
-                counter--;
             }
                 
         }
